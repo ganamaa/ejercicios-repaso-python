bd2d3760f5df7e133e53b592fded647ef627ae45
ganamaa <54151757+ganamaa@users.noreply.github.com>
2020-02-23 00:01:04 -0500

    Update and rename imparPar.py to ejercicio18.py

---
+++ ejercicio18.py
@@ -0,0 +1,6 @@
+#programa que determine si un número leído desde el teclado es par o impar
+num = int(input('ingrese un numero')) 
+if num % 2==0:
+  print("el numero es par") 
+else: 
+  print("el numero es impar")
--- imparPar.py
+++
@@ -1,5 +0,0 @@
-num = int(input('ingrese un numero')) 
-if num % 2==0:
-  print("el numero es par") 
-else: 
-  print("el numero es impar")
