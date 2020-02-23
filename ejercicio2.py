46cf0bafa26ba7b483c1b133118089193e396edc
ganamaa <54151757+ganamaa@users.noreply.github.com>
2020-02-23 00:20:25 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+# programa que pregunta al usuario su nombre y muestre en pantalla un saludo
+acompañado del nombre del usuario.
+nombre1 = (input('¿Cual es tu primer nombre?')) 
+nombre2 = (input('¿Cual es tu segundo nombre?')) 
+apellidos = (input('¿Cuales on tus apellidos?')) 
+
+print(f"Hola {nombre1} {nombre2} {apellidos})
