2e9f8769fffa4b997271879795df210ab7d4485d
ganamaa <54151757+ganamaa@users.noreply.github.com>
2020-02-24 23:05:14 -0500

    Update ejercicio81.py

--- modulos/ejercicio81.py
+++ modulos/ejercicio81.py
@@ -1,4 +1,6 @@
-# ejercicios con modulos .81
+"""módulo de funciones aritméticas que realicen las operaciones de suma, resta, 
+multiplicación, división y potencia de enteros, escribir un programa que importe este módulo 
+y use estas funciones para operar con números capturados desde el teclado."""
 import aritmetica as arit
 
 numeUno = int(input("ingrese un numero: "))
